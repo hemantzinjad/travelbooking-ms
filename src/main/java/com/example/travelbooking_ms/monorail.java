6f2deced0313fce019a05fb4fc3ba594157a747f
hemantzinjad <zinjadhr0304@gmail.com>
2025-11-23 15:24:54 +0530

    adding monorail

---
+++ src/main/java/com/example/travelbooking_ms/monorail.java
@@ -0,0 +1,13 @@
+package com.example.travelbooking_ms;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class monorail {
+    @GetMapping("/mymonorail")
+    public String getData() {
+        return "please book your train tickets from irctc";
+    }
+}
